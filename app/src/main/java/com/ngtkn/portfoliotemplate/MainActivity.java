0c72edbb05dc2a1a2b43139ee8ff259a9f0a75d9
ngt-kn <ngt.kn.dev@gmail.com>
2018-07-18 17:22:44 -0700

    do housekeeping

--- app/src/main/java/com/ngtkn/portfoliotemplate/MainActivity.java
+++ app/src/main/java/com/ngtkn/portfoliotemplate/MainActivity.java
@@ -1,16 +1,22 @@
 package com.ngtkn.portfoliotemplate;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.annotation.ColorInt;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ngtkn.portfoliotemplate.controller.AppPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tab_layout_id);
         tabs.setupWithViewPager(viewPager);
         tabs.setTabTextColors(Color.GRAY, Color.parseColor("#0069c0"));
+
+        textView = findViewById(R.id.contact_user);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNo = "tel:1234567890";
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(phoneNo)));
+            }
+        });
 
     }
 }
--- app/src/main/java/com/ngtkn/portfoliotemplate/views/AboutFragment.java
+++ app/src/main/java/com/ngtkn/portfoliotemplate/views/AboutFragment.java
@@ -24,7 +24,6 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
         View aboutView = inflater.inflate(R.layout.fragment_about, container, false);
 
